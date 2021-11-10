209439cb1e54b7ad3abffbb1519c592a3bbb36e4
K4LY4N007 <bskalyansrinivas@gmail.com>
2021-11-10 16:12:00 +0530

    day 1 assignment updated

--- day_1_assignment.py
+++ day_1_assignment.py
@@ -1,14 +1,36 @@
 a="the mahabharatha is the oldest mythology"
 lst=a.split()
 x=0
+y=0
 lst2=[]
-
+for i in a:
+    y=y+1
 for i in lst:
     x=x+1
 for i in lst:
     if i not in lst2:
         lst2.append(i)
+dic=dict()
+
 for i in range(0,len(lst2)):
     print('number of',lst2[i],'is:',lst.count(lst2[i]))
+    dic[lst2[i]]=lst.count(lst2[i])
+
+print('\n',dic)
     
 print('\nnumber of words',x)
+print('\nnumber of letters',y)
+
+
+
+
+###########################################
+
+a="The mahabharatha is the oldest mythology old"
+a=a.lower().split()
+
+{i:a.count(i) for i in set(a)}
+
+for i in set(a):
+    print(i, a.count(i))
+    
